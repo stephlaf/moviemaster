d70cd8612b8ae44699613407b533b362677ca5ed
Justin Bernold <jbernold@gmail.com>
2018-08-23 11:36:12 -0400

    Seedfile using Faker data sources

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
   belongs_to :level
+
+  def level_number
+    self.level.number
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  belongs_to :level
+  has_many :responses
+
+  def level_number
+    self.level.number
+  end
 end
