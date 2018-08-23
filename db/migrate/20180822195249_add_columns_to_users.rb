3a1cf78786fc8f55eb66763db1e896f23c3344c6
Stephane Lafontaine <stephane@medqigong.ca>
2018-08-23 16:35:05 -0400

    Styling navbar, fixed login form

--- db/migrate/20180822195249_add_columns_to_users.rb
+++ db/migrate/20180822195249_add_columns_to_users.rb
@@ -2,6 +2,6 @@ class AddColumnsToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_reference :users, :level, foreign_key: true
+    add_reference :users, :level, foreign_key: true, default: 1
   end
 end
