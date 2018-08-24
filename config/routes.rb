b88d661c94db36ca3d5e237309c87fcc26b99c29
Justin Bernold <jbernold@gmail.com>
2018-08-24 15:45:49 -0400

    wtf fucking goddammit!

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :quizzes, only: [:show]
+  resources :responses
 end
