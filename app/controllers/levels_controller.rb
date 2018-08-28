5d22ddf77d2e12c773ee5f51878ac0890b35cd0e
Stephane Lafontaine <stephane@medqigong.ca>
2018-08-28 10:25:34 -0400

    Added download function to movie notes button; js to make alert bars disappear after 2 sec

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -1,6 +1,16 @@
 class LevelsController < ApplicationController
   def show
     @level = Level.find_by(number: 2)
-    # @movie = 
+  end
+
+  def download_pdf
+    # send_file(
+    #   "#{Rails.root}/public/sw_movie_notes.pdf",
+    #   filename: "sw_movie_notes.pdf",
+    #   type: "application/pdf"
+    # )
+    pdf_filename = File.join(Rails.root, "public/sw_movie_notes.pdf")
+    send_file(pdf_filename, :filename => "sw_movie_notes.pdf", :disposition => 'inline', :type => "application/pdf")
+
   end
 end
