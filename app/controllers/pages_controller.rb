444602cc0d2f78ebf72eb51207d2eb1726424161
Justin Bernold <jbernold@gmail.com>
2018-08-29 15:54:52 -0400

    fixed annoying current_user_nil problem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    x = current_user.level.number + 1
+    x = 3
+    x = current_user.level.number + 1 if !current_user.nil?
     x =3 if x == 2
     @levels = Level.first(x)
     @lenses = Lens.first(x)
