333a26f289faa5352dcc15605e149a87787ddf9a
Justin Bernold <jbernold@gmail.com>
2018-08-29 15:32:44 -0400

    now levels are 'previously on movie master', 'now playing', or 'coming soon'

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @levels = Level.first(3)
-    @lenses = Lens.first(3)
+    x = current_user.level.number + 1
+    x =3 if x == 2
+    @levels = Level.first(x)
+    @lenses = Lens.first(x)
   end
 
   def end
